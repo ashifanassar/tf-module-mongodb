5d13321780d955403f1046083d5d923d668787a5
ashifanassar <ashifanassar@gmail.com>
2024-04-26 22:41:50 +0530

    terraform modules

--- docdb.tf
+++ docdb.tf
@@ -23,7 +23,7 @@ resource "aws_docdb_subnet_group" "docdb_" {
 resource "aws_docdb_cluster_instance" "cluster_instances" {
   count              = 1
   identifier         = "roboshop-${var.ENV}-docdb"
-  cluster_identifier = aws_docdb_cluster.default.id
+  cluster_identifier = aws_docdb_cluster.docdb.id
   instance_class     = "db.t3.medium"
 }
 
